86665a9d43dd187bf6fa6a166910d728b3b91b41
Voltios <ales.volivares@gmail.com>
2023-04-19 11:51:57 +0200

    cambios colores y footer

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+// let carousel = document.querySelector('.carousel');
+// let url = carousel.style.backgroundImage;
+// console.log(url);
+// var lista = [];
+// // lista.push(url.substring(5, url.length - 2));
+// lista.push(carousel.style.backgroundImage.substring(5, url.length - 2));
+// lista.push('https://images.unsplash.com/photo-1681797485212-7491d3f3619c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80');
+// lista.push('https://images.unsplash.com/photo-1454372182658-c712e4c5a1db?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80');
+// console.log(lista);
+// console.log(carousel);
+// let i = 1;
+// setInterval(() => {
+//     carousel.style.backgroundImage = 'url(' + lista[i] + ' )';
+
+//     i++;
+//     if (i > 2) i = 0;
+// }, 1000);
